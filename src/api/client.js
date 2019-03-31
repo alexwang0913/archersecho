6b7fc2d73284d73f0680ebf8aefd30a7245133a5
Ubuntu <guangwen1221@gmail.com>
2019-03-31 23:23:42 +0800

    change url

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+// axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL =
+  "http://ec2-18-223-119-0.us-east-2.compute.amazonaws.com";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 const client = async params => {
